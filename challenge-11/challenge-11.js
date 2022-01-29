cccaca84fe7869e7d9f81e53670e052c07d6cb9c
JVdoES <88451314+JVdoES@users.noreply.github.com>
2022-01-29 11:45:30 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -68,7 +68,20 @@ array chamado `numbers`. Se o contador for maior que 10, saia do loop.
 Mostre no console os números no array.
 */
 console.log( 'De 0 a 10:' );
-// ?
+
+ var numbers = [];
+
+  for(var i = 0; i <= 20; i++){
+      console.log(i);
+      numbers.push(i);
+      if(i >= 10){
+        console.log(numbers);
+        break;
+        
+      }
+    }
+
+
 
 /*
 Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
@@ -77,5 +90,18 @@ esses. Se o número for ímpar, pular para o próximo número.
 Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
-// ?
+ var numbers = [];
+
+  for(var i = 0; i <= 20; i++){
+
+    
+    if(i % 2 === 0){
+      console.log(i);
+      numbers.push(i);
+    }
+
+  }
+      console.log(numbers);
+
+
 }());  
